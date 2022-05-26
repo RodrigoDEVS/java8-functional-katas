ea7f576b9b6c8b464a5d1141b367e2ba46dd2dbd
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-05-25 23:52:20 -0500

    Kata Resuelta: completada la kata 8 y 9

--- src/main/java/katas/Kata8.java
+++ src/main/java/katas/Kata8.java
@@ -10,6 +10,8 @@ import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
     Goal: Combine videos and bookmarks by index (StreamUtils.zip) (https://github.com/poetix/protonpack)
@@ -21,8 +23,14 @@ public class Kata8 {
         List<Movie> movies = DataUtil.getMovies();
         List<Bookmark> bookMarks = DataUtil.getBookMarks();
 
+        Stream<String> marks = bookMarks.stream().map(element -> element.getId().toString());
+        Stream<String> pelis = movies.stream().map(list -> list.getId().toString());
+
+        List<Map> zipped = StreamUtils.zip(pelis, marks, (a, b) -> ImmutableMap.of("videoId", a, "bookmarkId", b)).collect(Collectors.toList());
+
+        System.out.println(zipped);
         // StreamUtils.zip()
 
-        return ImmutableList.of(ImmutableMap.of("videoId", 5, "bookmarkId", 3));
+        return zipped;
     }
 }
--- src/main/java/katas/Kata9.java
+++ src/main/java/katas/Kata9.java
@@ -7,9 +7,11 @@ import model.Movie;
 import model.MovieList;
 import util.DataUtil;
 
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve each video's id, title, middle interesting moment time, and smallest box art url
@@ -20,6 +22,22 @@ public class Kata9 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "some title", "time", new Date(), "url", "someUrl"));
+        int smallest = movieLists.stream().map(element -> element.getVideos())
+                .flatMap(Collection::stream)
+                .map(ele -> ele.getBoxarts())
+                .flatMap(List::stream)
+                .mapToInt(e -> e.getWidth())
+                .reduce(Integer::min)
+                .getAsInt();
+
+        List<Map> pelis = movieLists.stream().flatMap(list -> list.getVideos().stream())
+                .map(element -> ImmutableMap.of("id", element.getId(), "title",
+                        element.getTitle(), "time", new Date(),
+                        "url", element.getBoxarts().stream().filter(elem -> elem.getWidth() == smallest).map(ele -> ele.getUrl()).findAny().get()))
+                .collect(Collectors.toList());
+
+        System.out.println(pelis);
+
+        return pelis;
     }
 }
