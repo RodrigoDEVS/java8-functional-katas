cc85ccdfebfde50c853eea8de6ada682c049fdfe
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-05-25 19:40:47 -0500

    Kata Resuelta: solucionados los ejercicios 1 y 2

--- src/main/java/katas/Kata1.java
+++ src/main/java/katas/Kata1.java
@@ -5,8 +5,10 @@ import com.google.common.collect.ImmutableMap;
 import model.Movie;
 import util.DataUtil;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
     Goal: use map() to project an array of videos into an array of {id, title}-pairs
@@ -17,6 +19,16 @@ public class Kata1 {
     public static List<Map> execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys"));
+        //crear una nueva lista con un Map como argumento
+        List<Map> pelis = new ArrayList<>();
+
+        //llamar stream usando la lista "movies" que trae todo el arreglo inicial
+        pelis = movies.stream()
+                //recorrer el arreglo para obtener el id y el tittle
+                .map(element -> ImmutableMap.of(element.getId(), element.getTitle()))
+                //convertir el arreglo a una lista
+                .collect(Collectors.toList());
+
+        return pelis;
     }
 }
--- src/main/java/katas/Kata2.java
+++ src/main/java/katas/Kata2.java
@@ -7,6 +7,7 @@ import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Chain filter() and map() to collect the ids of videos that have a rating of 5.0
@@ -17,6 +18,12 @@ public class Kata2 {
     public static List<Integer> execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return ImmutableList.of(1, 2, 3);
+        List<Integer> pelis = movies.stream().filter(element -> element.getRating() == 5)
+                .map(element -> element.getId())
+                .collect(Collectors.toList());
+
+        System.out.println(pelis);
+
+        return pelis;
     }
 }
